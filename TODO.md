ce411b959959a77deec2d1481a7c6de1167861f6
evan-gan <139721374+evan-gan@users.noreply.github.com>
2025-01-02 19:45:59 -0500

    Add drivetrain, vision, and TODO.md

---
+++ TODO.md
@@ -0,0 +1,11 @@
+# To Do:
+- Install vendor deps
+    - Pathplanenr lib
+    - YAGSL lib
+    - Photon lib
+    - CTRE-Phoenix
+    - Choreo lib
+
+## After vendor deps:
+- Update swerve drive constants for this year's bot
+- Update AprilTagVison.java for current year's robot & resolve errors
---
+++ src/main/java/frc/robot/FieldConstants.java
@@ -0,0 +1,47 @@
+package frc.robot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
+public class FieldConstants {
+
+    public static final double FIELD_LENGTH = 16.54;
+    public static final double FIELD_WIDTH = 8.21;
+
+
+    public static boolean isRedAlliance() {
+        Optional<Alliance> alliance = DriverStation.getAlliance();
+        return alliance.isPresent() && alliance.get() == Alliance.Red;
+    }
+
+    public static Pose2d flipPose(Pose2d pose) {
+        // flip pose when red
+        if (isRedAlliance()) {
+            Rotation2d rot = pose.getRotation();
+            // reflect the pose over center line, flip both the X and the rotation
+            return new Pose2d(FIELD_LENGTH - pose.getX(), pose.getY(), new Rotation2d(-rot.getCos(), rot.getSin()));
+        }
+
+        // Blue or we don't know; return the original pose
+        return pose;
+    }
+
+    public static Translation2d flipTranslation(Translation2d position) {
+        // flip when red
+        if (isRedAlliance()) {
+            // reflect the pose over center line, flip both the X
+            return new Translation2d(FIELD_LENGTH - position.getX(), position.getY());
+        }
+
+        // Blue or we don't know; return the original position
+        return position;
+    }
+}
