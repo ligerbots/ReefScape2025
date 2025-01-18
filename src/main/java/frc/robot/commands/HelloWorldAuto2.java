43da4fc6ba66174de69eb27e7a8a88e46f88a6fe
LigerBots <ligerbots@gmail.com>
2025-01-18 12:58:14 -0500

    Timeouts

--- src/main/java/frc/robot/commands/HelloWorldAuto2.java
+++ src/main/java/frc/robot/commands/HelloWorldAuto2.java
@@ -56,24 +56,24 @@ public class HelloWorldAuto2 extends AutoCommandInterface {
                 m_initPose = startPath.getStartingDifferentialPose();
                 
                 addCommands(m_driveTrain.followPath(startPath));
-                addCommands(new StartEndCommand(roller::runRollerOut, roller::stop, roller).withTimeout(.25));
+                addCommands(new StartEndCommand(roller::runRollerOut, roller::stop, roller).withTimeout(.3));
 
                 addCommands(m_driveTrain.followPath(PathPlannerPath.fromPathFile("Shot1 to Source1")));
-                addCommands(new WaitCommand(.25));
+                // addCommands(new WaitCommand(.1));
                 addCommands(m_driveTrain.followPath(PathPlannerPath.fromPathFile("Source1 to Shot2")));
-                addCommands(new StartEndCommand(roller::runRollerOut, roller::stop, roller).withTimeout(.25));
+                addCommands(new StartEndCommand(roller::runRollerOut, roller::stop, roller).withTimeout(.3));
                 ;
 
                 addCommands(m_driveTrain.followPath(PathPlannerPath.fromPathFile("Shot2 to Source1")));
-                addCommands(new WaitCommand(.25));
+                // addCommands(new WaitCommand(.1));
 
                 addCommands(m_driveTrain.followPath(PathPlannerPath.fromPathFile("Source1 to ReefB")));
-                addCommands(new StartEndCommand(roller::runRollerOut, roller::stop, roller).withTimeout(.25));
+                addCommands(new StartEndCommand(roller::runRollerOut, roller::stop, roller).withTimeout(.3));
 
                 addCommands(m_driveTrain.followPath(PathPlannerPath.fromPathFile("ReefB to Source1")));
-                addCommands(new WaitCommand(.25));
+                // addCommands(new WaitCommand(.1));
                 addCommands(m_driveTrain.followPath(PathPlannerPath.fromPathFile("Source1 to ReefB")));
-                addCommands(new StartEndCommand(roller::runRollerOut, roller::stop, roller).withTimeout(.25));
+                addCommands(new StartEndCommand(roller::runRollerOut, roller::stop, roller).withTimeout(.3));
             }
 
         } catch (Exception e) {
