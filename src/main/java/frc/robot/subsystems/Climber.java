5390814c4567ef6a4a15a9043d9286f3af1bbefb
coachfitz0807 <j-fitz@rcn.com>
2025-02-06 21:27:06 -0500

    faked climber, now is a falcon

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,20 +1,18 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.SparkBase.PersistMode;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Climber extends SubsystemBase {
     
-    private final SparkMax m_climberMotor;
-    private final RelativeEncoder m_climberMotorEncoder;
+    private final TalonFX m_climberMotor;
+    // private final RelativeEncoder m_climberMotorEncoder;
     
     // Constants to be used in this class
     private static final double DEPLOYED_ROTATIONS = -10.0;
@@ -24,7 +22,7 @@ public class Climber extends SubsystemBase {
     private static final double MAX_WINCH_ROTATIONS_ALLOWED = 70.0;
     private static final double MAX_WINCH_CURRENT = 100.0;
     // Current limit in the SparkMax
-    private static final int WINCH_CURRENT_LIMIT = 40;
+    private static final double WINCH_CURRENT_LIMIT = 40;
 
     // Winch motor speed values
     private static final double IDLE_MOTOR_SPEED = -0.01;
@@ -53,73 +51,70 @@ public class Climber extends SubsystemBase {
     private ClimberState m_climberState = ClimberState.IDLE;
     
     public Climber() {
-        m_climberMotor = new SparkMax(Constants.CLIMBER_ID, MotorType.kBrushless);
+        m_climberMotor = new TalonFX(Constants.CLIMBER_ID);
+
+        TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();
+        CurrentLimitsConfigs currentLimits = new CurrentLimitsConfigs().withSupplyCurrentLimit(WINCH_CURRENT_LIMIT);
+        talonFXConfigs.withCurrentLimits(currentLimits);
         
-        SparkMaxConfig config = new SparkMaxConfig();
+        m_climberMotor.getConfigurator().apply(talonFXConfigs);
         
-        config.voltageCompensation(MOTOR_VOLTAGE_COMP);
-        config.smartCurrentLimit(WINCH_CURRENT_LIMIT);
-        config.inverted(false);//guess 
-        config.idleMode(IdleMode.kBrake);
-        // m_climberMotor.setCANTimeout(250);
-        
-        m_climberMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-        
-        m_climberMotorEncoder = m_climberMotor.getEncoder();
-        m_climberMotorEncoder.setPosition(0.0);
+        // m_climberMotorEncoder = m_climberMotor.getEncoder();
+        // m_climberMotorEncoder.setPosition(0.0);
     }
     
     @Override
     public void periodic() {
-        double position = m_climberMotorEncoder.getPosition();
+        double position = m_climberMotor.getPosition().getValueAsDouble();
         
-        double climberCurrrent = m_climberMotor.getOutputCurrent();
+        double climberCurrrent = m_climberMotor.getSupplyCurrent().getValueAsDouble();
         
+        SmartDashboard.putNumber("climber/supplyCurrent", climberCurrrent);
+        SmartDashboard.putNumber("climber/statorCurrent", m_climberMotor.getStatorCurrent().getValueAsDouble());
         SmartDashboard.putNumber("climber/Speed", m_climberMotor.get());
         SmartDashboard.putNumber("climber/Position", position);
-        SmartDashboard.putNumber("climber/Current", climberCurrrent);
         SmartDashboard.putString("climber/state", m_climberState.toString());
         
-        // While idle, we want a small voltage applied to hold the hooks in place.
-        if (m_climberState == ClimberState.IDLE) {
-            m_climberMotor.set(IDLE_MOTOR_SPEED);
-            // Note that the DEPLOYING state is entered via a command, so here we just stay IDLE
-        }
-        else if (m_climberState == ClimberState.DEPLOYING) {
-            // use a "trapezoidal" profile to slow down near the end
+        // // While idle, we want a small voltage applied to hold the hooks in place.
+        // if (m_climberState == ClimberState.IDLE) {
+        //     // m_climberMotor.set(IDLE_MOTOR_SPEED);
+        //     // Note that the DEPLOYING state is entered via a command, so here we just stay IDLE
+        // }
+        // else if (m_climberState == ClimberState.DEPLOYING) {
+        //     // use a "trapezoidal" profile to slow down near the end
             
-            // Deploy goes NEGATIVE!!
-            // double error = Math.abs(DEPLOYED_ROTATIONS - position);
-            if (position <= DEPLOYED_ROTATIONS) {
-                // Stop winch
-                m_climberMotor.set(0.0);
-                m_climberState = ClimberState.WAITING;
-            }
-            // else {
-            //     // slow down over the last N rotations
-            //     double speed =  error / EXTEND_SLOWDOWN_INTERVAL * WINCH_EXTEND_MAX_SPEED;
-            //     m_climberMotor.set(MathUtil.clamp(speed, WINCH_EXTEND_MIN_SPEED, WINCH_EXTEND_MAX_SPEED));
-            // }
-        }
-        else if (m_climberState == ClimberState.WAITING) {
-            // Nothing to do here since both winches are stopped and the hooks are as high as they can go.
-            // The CLIMBING state is only entered via a command
-        }
-        else if (m_climberState == ClimberState.CLIMBING) {
-            m_climberMotor.set(WINCH_CLIMB_SPEED);
-        }
-        else if (m_climberState == ClimberState.CLIMBING) {          
-            if (position >= CLIMB_ROTATIONS) {
-                m_climberMotor.set(0.0);
-                m_climberState = ClimberState.HOLDING;
-            }
-        }
+        //     // Deploy goes NEGATIVE!!
+        //     // double error = Math.abs(DEPLOYED_ROTATIONS - position);
+        //     if (position <= DEPLOYED_ROTATIONS) {
+        //         // Stop winch
+        //         m_climberMotor.set(0.0);
+        //         m_climberState = ClimberState.WAITING;
+        //     }
+        //     // else {
+        //     //     // slow down over the last N rotations
+        //     //     double speed =  error / EXTEND_SLOWDOWN_INTERVAL * WINCH_EXTEND_MAX_SPEED;
+        //     //     m_climberMotor.set(MathUtil.clamp(speed, WINCH_EXTEND_MIN_SPEED, WINCH_EXTEND_MAX_SPEED));
+        //     // }
+        // }
+        // else if (m_climberState == ClimberState.WAITING) {
+        //     // Nothing to do here since both winches are stopped and the hooks are as high as they can go.
+        //     // The CLIMBING state is only entered via a command
+        // }
+        // else if (m_climberState == ClimberState.CLIMBING) {
+        //     m_climberMotor.set(WINCH_CLIMB_SPEED);
+        // }
+        // else if (m_climberState == ClimberState.CLIMBING) {          
+        //     if (position >= CLIMB_ROTATIONS) {
+        //         m_climberMotor.set(0.0);
+        //         m_climberState = ClimberState.HOLDING;
+        //     }
+        // }
         
-        // Protection: stop if position is at max or current is too high
-        // Test no matter what State we are in. 
-        // Do this at the end to override any settings
-        if (position >= MAX_WINCH_ROTATIONS_ALLOWED || climberCurrrent > MAX_WINCH_CURRENT)
-            m_climberMotor.set(0.0);
+        // // Protection: stop if position is at max or current is too high
+        // // Test no matter what State we are in. 
+        // // Do this at the end to override any settings
+        // if (position >= MAX_WINCH_ROTATIONS_ALLOWED || climberCurrrent > MAX_WINCH_CURRENT)
+        //     m_climberMotor.set(0.0);
     }
     
     private double limitWinch(RelativeEncoder encoder, double speed) {
@@ -130,15 +125,16 @@ public class Climber extends SubsystemBase {
     }
     
     public void run(double climbSpeed) {
-        m_climberMotor.set(limitWinch(m_climberMotorEncoder, climbSpeed));
+        // m_climberMotor.set(limitWinch(m_climberMotorEncoder, climbSpeed));
+        m_climberMotor.set( climbSpeed);
     }
     
     public double getPosition(){
-        return m_climberMotorEncoder.getPosition();
+        return m_climberMotor.getPosition().getValueAsDouble();
     }
     
     public double getVelocity(){
-        return m_climberMotorEncoder.getVelocity();
+        return m_climberMotor.getVelocity().getValueAsDouble();
     }
     
     public void deploy() {
