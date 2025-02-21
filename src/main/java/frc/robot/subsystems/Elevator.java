54ca8a4047b2c04aac68ef440096015fa92477c3
LigerBots <ligerbots@gmail.com>
2025-02-21 16:02:34 -0500

    slight elevator tuning

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -44,8 +44,8 @@ public class Elevator extends SubsystemBase {
     private static final double MIN_HEIGHT_TURN_OFF = Units.inchesToMeters(1.0);
 
     // TODO set to good values
-    private static final double MAX_VEL_METER_PER_SEC = Units.inchesToMeters(90.0);
-    private static final double MAX_ACC_METER_PER_SEC_SQ = Units.inchesToMeters(110.0);
+    private static final double MAX_VEL_METER_PER_SEC = Units.inchesToMeters(170.0);
+    private static final double MAX_ACC_METER_PER_SEC_SQ = Units.inchesToMeters(300.0);
     private static final double MAX_JERK_METER_PER_SEC3 = Units.inchesToMeters(1000.0);
     
     private static final int CURRENT_LIMIT = 30;
@@ -53,7 +53,8 @@ public class Elevator extends SubsystemBase {
     private static final double OFFSET_METER = 0.0;
 
     private static final double STATIC_VOLTAGE = 0.6;
-  
+    private static final double K_P = 0.5;
+
     // private final int POTENTIOMETER_CHANNEL = 2; //TODO: Update with actual value
     // private final double POTENTIOMETER_RANGE_METERS = -2.625; // meters, the string potentiometer on takes in range in integers TODO: update to correct value
     // private final double POTENTIOMETER_OFFSET = 2.51; //TODO: Find inital value and update
@@ -80,7 +81,7 @@ public class Elevator extends SubsystemBase {
         slot0configs.kV = 0.0; // A velocity target of 1 rps results in 0.12 V output
         slot0configs.kA = 0.0; // An acceleration of 1 rps/s requires 0.01 V output
         // m_slot0configs.kP = 4.8; // A position error of 2.5 rotations results in 12 V output
-        slot0configs.kP = 0.5;  // start small!!!
+        slot0configs.kP = K_P;  // start small!!!
         slot0configs.kI = 0.0; // no output for integrated error
         slot0configs.kD = 0.0; // A velocity error of 1 rps results in 0.1 V output
         
