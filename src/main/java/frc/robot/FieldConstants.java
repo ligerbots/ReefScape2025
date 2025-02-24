a020a64acdf185e251e6cfa0aecd696358262642
Paul Rensing <prensing@ligerbots.org>
2025-02-23 20:38:19 -0500

    Adjust Reef scoring angles

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -18,20 +18,20 @@ public class FieldConstants {
 
     public static final Pose2d ROBOT_START_1 = new Pose2d(7.29, 1.64, Rotation2d.fromDegrees(150.6));
     public static final Pose2d ROBOT_START_2 = new Pose2d(7.23, 4.20, Rotation2d.fromDegrees(180));
-    public static final Pose2d ROBOT_START_3 = new Pose2d(7.29,6.75, Rotation2d.fromDegrees(-149.9));  
+    public static final Pose2d ROBOT_START_3 = new Pose2d(7.29, 6.75, Rotation2d.fromDegrees(-149.9));  
 
     public static final Pose2d REEF_A = new Pose2d(3.19,4.25, Rotation2d.fromDegrees(0));
     public static final Pose2d REEF_B = new Pose2d(3.19,3.86, Rotation2d.fromDegrees(0));
-    public static final Pose2d REEF_C = new Pose2d(3.71,3.00, Rotation2d.fromDegrees(58.4));
-    public static final Pose2d REEF_D = new Pose2d(3.98,2.85, Rotation2d.fromDegrees(58.4));
-    public static final Pose2d REEF_E = new Pose2d(4.99,2.85, Rotation2d.fromDegrees(120.9));
-    public static final Pose2d REEF_F = new Pose2d(5.25,3.00, Rotation2d.fromDegrees(120.9));
+    public static final Pose2d REEF_C = new Pose2d(3.71,3.00, Rotation2d.fromDegrees(60.0));
+    public static final Pose2d REEF_D = new Pose2d(3.98,2.85, Rotation2d.fromDegrees(60.0));
+    public static final Pose2d REEF_E = new Pose2d(4.99,2.85, Rotation2d.fromDegrees(120.0));
+    public static final Pose2d REEF_F = new Pose2d(5.25,3.00, Rotation2d.fromDegrees(120.0));
     public static final Pose2d REEF_G = new Pose2d(5.77,3.87, Rotation2d.fromDegrees(180.0));
     public static final Pose2d REEF_H = new Pose2d(5.77,4.20, Rotation2d.fromDegrees(180.0));
-    public static final Pose2d REEF_I = new Pose2d(5.23,5.08, Rotation2d.fromDegrees(-119.9));
-    public static final Pose2d REEF_J = new Pose2d(4.98,5.21, Rotation2d.fromDegrees(-119.9));
-    public static final Pose2d REEF_K = new Pose2d(3.98,5.20, Rotation2d.fromDegrees(-59.9));
-    public static final Pose2d REEF_L = new Pose2d(3.70,5.06, Rotation2d.fromDegrees(-59.9));
+    public static final Pose2d REEF_I = new Pose2d(5.23,5.08, Rotation2d.fromDegrees(-120.0));
+    public static final Pose2d REEF_J = new Pose2d(4.98,5.21, Rotation2d.fromDegrees(-120.0));
+    public static final Pose2d REEF_K = new Pose2d(3.98,5.20, Rotation2d.fromDegrees(-60.0));
+    public static final Pose2d REEF_L = new Pose2d(3.70,5.06, Rotation2d.fromDegrees(-60.0));
 
     public static final Pose2d SOURCE_1_IN = new Pose2d(0.64,1.37, Rotation2d.fromDegrees(55.6));
     public static final Pose2d SOURCE_1_OUT = new Pose2d(1.65,0.64, Rotation2d.fromDegrees(55.6));
