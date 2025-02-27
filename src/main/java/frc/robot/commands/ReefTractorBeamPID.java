cfec6e53151ba50d4711b85921e385e67eb3781d
evan-gan <139721374+evan-gan@users.noreply.github.com>
2025-02-27 07:21:36 -0500

    Add fixed position for ReefTractorBeamPID for testing

--- src/main/java/frc/robot/commands/ReefTractorBeamPID.java
+++ src/main/java/frc/robot/commands/ReefTractorBeamPID.java
@@ -6,6 +6,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.FieldConstants;
@@ -15,13 +16,14 @@ import frc.robot.subsystems.DriveTrain;
 public class ReefTractorBeamPID extends Command {
     /** Creates a new ReefTractorBeamPID. */
     private final DriveTrain m_driveTrain;
-    private final double m_P = 1;
+    private final double m_P = 0.5;
     private final double m_I = 0;
     private final double m_D = 0;
     private final PIDController m_pid = new PIDController(m_P, m_I, m_D);
     private double m_tolorence;
     private Pose2d m_currentPose;
     private Pose2d m_nearestPole;
+    private long m_startTime;
 
     public ReefTractorBeamPID(DriveTrain drivetrain) {
         // Use addRequirements() here to declare subsystem dependencies.
@@ -33,14 +35,16 @@ public class ReefTractorBeamPID extends Command {
     @Override
     public void initialize() {
         m_currentPose = m_driveTrain.getPose();
-        m_nearestPole = FieldConstants.flipPose(FieldConstants.flipPose(m_currentPose).nearest(FieldConstants.REEF_SCORING_LOCATIONS));
+        m_startTime = System.currentTimeMillis();
+        // m_nearestPole = FieldConstants.flipPose(FieldConstants.flipPose(m_currentPose).nearest(FieldConstants.REEF_SCORING_LOCATIONS));
+        m_nearestPole = new Pose2d(9.31, 0.80, Rotation2d.fromDegrees(-180));
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
         m_currentPose = m_driveTrain.getPose();
-
+        // System.out.println("Current Pose: " + m_currentPose);
         double translationX = -m_pid.calculate(m_currentPose.getX(), m_nearestPole.getX());
         double translationY = -m_pid.calculate(m_currentPose.getY(), m_nearestPole.getY());
 
@@ -52,11 +56,13 @@ public class ReefTractorBeamPID extends Command {
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
+        System.out.println("Done! \nCurrent pose:" + m_currentPose + "\nGoal pose:" + m_nearestPole + "\nTime: " + (System.currentTimeMillis() - m_startTime) / 1000.0);
     }
 
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return m_tolorence < 0.01 && 0 != m_tolorence;
+        boolean isWithinTolorence = m_tolorence < 0.001;
+        return isWithinTolorence;
     }
 }
