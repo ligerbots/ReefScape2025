f9fe6642f669cbd63b9110bc0e13236f05c087e3
Paul Rensing <prensing@ligerbots.org>
2025-03-05 14:05:39 -0500

    fix back camera location

--- src/main/java/frc/robot/subsystems/AprilTagVision.java
+++ src/main/java/frc/robot/subsystems/AprilTagVision.java
@@ -148,7 +148,7 @@ public class AprilTagVision extends SubsystemBase {
             ));
 
         // m_cameras[Cam.BACK.idx] = new Camera("ArducamBack", new Transform3d(
-        //         new Translation3d(Units.inchesToMeters(-9.5), Units.inchesToMeters(10.75), Units.inchesToMeters(9.0)),
+        //         new Translation3d(Units.inchesToMeters(-9.85), Units.inchesToMeters(11.05), Units.inchesToMeters(9.3)),
         //         new Rotation3d(0.0, Math.toRadians(-20), 0)
         //             .rotateBy(new Rotation3d(0, 0, Math.toRadians(180)))
         //         ));
