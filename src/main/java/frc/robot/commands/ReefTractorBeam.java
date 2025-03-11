58b7f8c332fa078d76b37df1ac12f93cd5d80276
LigerBots <ligerbots@gmail.com>
2025-03-11 19:33:41 -0400

    more constant to FieldConstants

--- src/main/java/frc/robot/commands/ReefTractorBeam.java
+++ src/main/java/frc/robot/commands/ReefTractorBeam.java
@@ -1,6 +1,5 @@
 package frc.robot.commands;
 
-import java.util.List;
 import java.util.function.Supplier;
 
 import com.pathplanner.lib.path.PathConstraints;
@@ -11,11 +10,6 @@ import frc.robot.FieldConstants;
 import frc.robot.subsystems.DriveTrain;
 
 public class ReefTractorBeam implements Supplier<Command> {
-
-    private static final List<Pose2d> REEF_POLES = List.of( FieldConstants.REEF_A, FieldConstants.REEF_B, FieldConstants.REEF_C, 
-                                                            FieldConstants.REEF_D, FieldConstants.REEF_E, FieldConstants.REEF_F, 
-                                                            FieldConstants.REEF_G, FieldConstants.REEF_H, FieldConstants.REEF_I, 
-                                                            FieldConstants.REEF_J, FieldConstants.REEF_K, FieldConstants.REEF_L);
 
     private DriveTrain m_driveTrain;
 
@@ -32,7 +26,7 @@ public class ReefTractorBeam implements Supplier<Command> {
         System.out.println("Current Pose: " + currentPose);
         // SmartDashboard.putData("Current Pose:", currentPose);
 
-        Pose2d nearestPole = FieldConstants.flipPose(currentPose.nearest(REEF_POLES));
+        Pose2d nearestPole = FieldConstants.flipPose(currentPose.nearest(FieldConstants.REEF_POLES));
         System.out.println("Nearest Pole: " + nearestPole);
         return m_driveTrain.pathFindToPose(nearestPole, constraints);
     }
