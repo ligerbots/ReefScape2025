c00546eeed35a0c7472973d535f905da6b831639
Paul Rensing <prensing@ligerbots.org>
2025-03-12 11:24:15 -0400

    add Reef Algae positions

--- reef_positions.py
+++ reef_positions.py
@@ -12,6 +12,8 @@ center_algae_offset = 4.0
 
 letter = 'A'
 
+algae = []
+
 for i in range(-3, 3):
     angle = i * 60.0
     robot_angle = (angle + 180.0) % 360.0
@@ -20,26 +22,21 @@ for i in range(-3, 3):
 
     rad = math.radians(angle)
     point1 = (center[0] + math.cos(rad)*center_offset, center[1] + math.sin(rad)*center_offset)
+
+    letter2 = chr(ord(letter) + 1)
+    algae_pos = (meters(point1[0] + math.cos(rad)*center_algae_offset), meters(point1[1] + math.sin(rad)*center_algae_offset))
+    algae.append(f"    public static final Pose2d REEF_ALGAE_{letter}{letter2} = new Pose2d({algae_pos[0]:0.4}, {algae_pos[1]:0.4}, Rotation2d.fromDegrees({robot_angle}));")
 
     for j in (-1, 1):
         rad2 = math.radians(angle + j * 90.0)
         point2 = (meters(point1[0] + math.cos(rad2)*reef_offset),
                   meters(point1[1] + math.sin(rad2)*reef_offset))
-        print(f"    public static final Pose2d REEF_{letter} = new Pose2d({point2[0]:0.3}, {point2[1]:0.3}, Rotation2d.fromDegrees({robot_angle}));")
+        print(f"    public static final Pose2d REEF_{letter} = new Pose2d({point2[0]:0.4}, {point2[1]:0.4}, Rotation2d.fromDegrees({robot_angle}));")
 
         letter = chr(ord(letter) + 1)
 
-letter = 'A'
-for i in range(-3, 3):
-    angle = i * 60.0
-    robot_angle = (angle + 180.0) % 360.0
-    if robot_angle > 180:
-        robot_angle -= 360
+print()
+for m in algae:
+    print(m)
 
-    letter2 = chr(ord(letter) + 1)
-
-    rad = math.radians(angle)
-    point1 = (center[0] + math.cos(rad)*center_offset, center[1] + math.sin(rad)*center_offset)
-    print(f"    public static final Pose2d REEF_CENTER_{letter}{letter2} = new Pose2d({point2[0]:0.3}, {point2[1]:0.3}, Rotation2d.fromDegrees({robot_angle}));")
-
-    letter = chr(ord(letter) + 2)
+# TODO: add the CoralStation positions
--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -20,18 +20,27 @@ public class FieldConstants {
     public static final Pose2d ROBOT_START_2 = new Pose2d(7.23, 4.20, Rotation2d.fromDegrees(180));
     public static final Pose2d ROBOT_START_3 = new Pose2d(7.29, 6.75, Rotation2d.fromDegrees(-149.9));  
 
-    public static final Pose2d REEF_A = new Pose2d(3.21, 4.19, Rotation2d.fromDegrees(0.0));
-    public static final Pose2d REEF_B = new Pose2d(3.21, 3.86, Rotation2d.fromDegrees(0.0));
-    public static final Pose2d REEF_C = new Pose2d(3.71, 3.00, Rotation2d.fromDegrees(60.0));
-    public static final Pose2d REEF_D = new Pose2d(3.99, 2.83, Rotation2d.fromDegrees(60.0));
-    public static final Pose2d REEF_E = new Pose2d(4.98, 2.83, Rotation2d.fromDegrees(120.0));
-    public static final Pose2d REEF_F = new Pose2d(5.27, 3.00, Rotation2d.fromDegrees(120.0));
-    public static final Pose2d REEF_G = new Pose2d(5.77, 3.86, Rotation2d.fromDegrees(180.0));
-    public static final Pose2d REEF_H = new Pose2d(5.77, 4.19, Rotation2d.fromDegrees(180.0));
-    public static final Pose2d REEF_I = new Pose2d(5.27, 5.04, Rotation2d.fromDegrees(-120.0));
-    public static final Pose2d REEF_J = new Pose2d(4.98, 5.21, Rotation2d.fromDegrees(-120.0));
-    public static final Pose2d REEF_K = new Pose2d(3.99, 5.21, Rotation2d.fromDegrees(-60.0));
-    public static final Pose2d REEF_L = new Pose2d(3.71, 5.04, Rotation2d.fromDegrees(-60.0));
+    public static final Pose2d REEF_A = new Pose2d(3.213, 4.186, Rotation2d.fromDegrees(0.0));
+    public static final Pose2d REEF_B = new Pose2d(3.213, 3.856, Rotation2d.fromDegrees(0.0));
+    public static final Pose2d REEF_C = new Pose2d(3.708, 2.998, Rotation2d.fromDegrees(60.0));
+    public static final Pose2d REEF_D = new Pose2d(3.994, 2.833, Rotation2d.fromDegrees(60.0));
+    public static final Pose2d REEF_E = new Pose2d(4.985, 2.833, Rotation2d.fromDegrees(120.0));
+    public static final Pose2d REEF_F = new Pose2d(5.271, 2.998, Rotation2d.fromDegrees(120.0));
+    public static final Pose2d REEF_G = new Pose2d(5.766, 3.856, Rotation2d.fromDegrees(180.0));
+    public static final Pose2d REEF_H = new Pose2d(5.766, 4.186, Rotation2d.fromDegrees(180.0));
+    public static final Pose2d REEF_I = new Pose2d(5.271, 5.044, Rotation2d.fromDegrees(-120.0));
+    public static final Pose2d REEF_J = new Pose2d(4.985, 5.209, Rotation2d.fromDegrees(-120.0));
+    public static final Pose2d REEF_K = new Pose2d(3.994, 5.209, Rotation2d.fromDegrees(-60.0));
+    public static final Pose2d REEF_L = new Pose2d(3.708, 5.044, Rotation2d.fromDegrees(-60.0));
+
+    // Note: these positions are 4 inches back from touching the bumpers touching the wall
+    //   because we need to move the EE before driving all the way in.
+    public static final Pose2d REEF_ALGAE_AB = new Pose2d(3.111, 4.021, Rotation2d.fromDegrees(0.0));
+    public static final Pose2d REEF_ALGAE_CD = new Pose2d(3.8, 2.827, Rotation2d.fromDegrees(60.0));
+    public static final Pose2d REEF_ALGAE_EF = new Pose2d(5.178, 2.827, Rotation2d.fromDegrees(120.0));
+    public static final Pose2d REEF_ALGAE_GH = new Pose2d(5.867, 4.021, Rotation2d.fromDegrees(180.0));
+    public static final Pose2d REEF_ALGAE_IJ = new Pose2d(5.178, 5.214, Rotation2d.fromDegrees(-120.0));
+    public static final Pose2d REEF_ALGAE_KL = new Pose2d(3.8, 5.214, Rotation2d.fromDegrees(-60.0));
 
     public static final Pose2d SOURCE_1_IN = new Pose2d(0.64, 1.37, Rotation2d.fromDegrees(55.6));
     public static final Pose2d SOURCE_1_OUT = new Pose2d(1.65, 0.64, Rotation2d.fromDegrees(55.6));
