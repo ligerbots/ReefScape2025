0d7c431e4dc05f317f139d3d95dd09c03e1821ce
jacobh1 <jake.hendrickson@gmail.com>
2025-03-15 16:21:26 -0400

    Clean up mirroring of target pose

--- src/main/java/frc/robot/commands/CompBotExperimentalAutoRefactor.java
+++ src/main/java/frc/robot/commands/CompBotExperimentalAutoRefactor.java
@@ -30,7 +30,7 @@ public class CompBotExperimentalAutoRefactor extends ReefscapeAbstractAuto {
     private static final double RAISE_ELEVATOR_AFTER_PATH_START = 1.0;
     private static final double START_INTAKE_AFTER_PATH_START = 0.5;
     
-
+    private Pose2d m_sourcePoint;
     private DriveTrain m_driveTrain;
     private Elevator m_elevator;
     private CoralEffector m_coralEffector;
@@ -40,17 +40,17 @@ public class CompBotExperimentalAutoRefactor extends ReefscapeAbstractAuto {
     PathConstraints constraints =  new PathConstraints(
     4.0, 2.0,
     Math.toRadians(540), Math.toRadians(720));
-
-    /** Creates a new NoteAuto. */
+    
     public CompBotExperimentalAutoRefactor(Pose2d startPoint, Pose2d sourcePoint, Pose2d[] reefPoints, DriveTrain driveTrain, 
-    Elevator elevator, CoralEffector coralEffector, EndEffectorPivot pivot, boolean isProcessorSide) {
-        super(startPoint, sourcePoint, reefPoints, driveTrain, elevator, coralEffector, pivot, isProcessorSide);
-        m_driveTrain = driveTrain;
-        m_elevator = elevator;
-        m_coralEffector = coralEffector;
-        m_pivot = pivot;
-        m_isProcessorSide = isProcessorSide;
-        
+        Elevator elevator, CoralEffector coralEffector, EndEffectorPivot pivot, boolean isProcessorSide) {
+            super(startPoint, sourcePoint, reefPoints, driveTrain, elevator, coralEffector, pivot, isProcessorSide);
+            m_sourcePoint = sourcePoint;
+            m_driveTrain = driveTrain;
+            m_elevator = elevator;
+            m_coralEffector = coralEffector;
+            m_pivot = pivot;
+            m_isProcessorSide = isProcessorSide;
+            
         if (Robot.isSimulation()) {
             CORAL_PICKUP_WAIT_TIME = 1.0;
         } else {
@@ -62,20 +62,16 @@ public class CompBotExperimentalAutoRefactor extends ReefscapeAbstractAuto {
             
             m_initPose = startPath.getStartingDifferentialPose();
             
-            // addCommands(m_driveTrain.followPath(PathFactory.getPath(sourcePoint, reefPoints[1], isProcessorSide))
-            Pose2d reefPoint1 = isProcessorSide ? FieldConstants.mirrorPose(reefPoints[1]) : reefPoints[1];
-            Pose2d reefPoint2 = isProcessorSide ? FieldConstants.mirrorPose(reefPoints[2]) : reefPoints[2];
-            Pose2d reefPoint3 = isProcessorSide ? FieldConstants.mirrorPose(reefPoints[3]) : reefPoints[3];
-
             addCommands(m_driveTrain.followPath(startPath).alongWith(
                 new WaitCommand(.5).andThen(
                     new MoveEndEffector(Constants.Position.L4, elevator, pivot, RAISE_ELEVATOR_WAIT_TIME))));
-            addCommands(new StartEndCommand(coralEffector::runOuttake, coralEffector::stop, coralEffector).withTimeout(CORAL_SCORE_WAIT_TIME));                
+            addCommands(new StartEndCommand(coralEffector::runOuttake, coralEffector::stop, coralEffector).withTimeout(CORAL_SCORE_WAIT_TIME));  
+                          
             if (reefPoints.length > 1) {
                 addCommands(
-                    pickupCorralThenScoreL4(reefPoints[0], sourcePoint, "Source2Center to ReefApproachK", reefPoint1),
-                    pickupCorralThenScoreL4(reefPoints[1], sourcePoint, "Source2Center to ReefApproachL", reefPoint2),
-                    pickupCorralThenScoreL4(reefPoints[2], sourcePoint, "Source2Center to ReefApproachA", reefPoint3)
+                    pickupCorralThenScoreL4(reefPoints[0], "Source2Center to ReefApproachK", reefPoints[1]),
+                    pickupCorralThenScoreL4(reefPoints[1], "Source2Center to ReefApproachL", reefPoints[2]),
+                    pickupCorralThenScoreL4(reefPoints[2], "Source2Center to ReefApproachA", reefPoints[3])
                 );
         }
             
@@ -84,11 +80,16 @@ public class CompBotExperimentalAutoRefactor extends ReefscapeAbstractAuto {
             m_initPose = new Pose2d();
         }
     }
+
+    private Pose2d mirrorIfNeeded(Pose2d pose) {
+        return m_isProcessorSide ? FieldConstants.mirrorPose(pose) : pose;
+    }
     
-    private Command pickupCorralThenScoreL4(Pose2d driveStartPoint, Pose2d sourcePoint, String approachPath, Pose2d targetScore) {
+    private Command pickupCorralThenScoreL4(Pose2d driveStartPoint, String approachPath, Pose2d targetScore) {
+        targetScore = mirrorIfNeeded(targetScore);
         return Commands.sequence(
                     Commands.parallel(new MoveEndEffector(Constants.Position.BACK_INTAKE, m_elevator, m_pivot, LOWER_ELEVATOR_WAIT_TIME),
-                        Commands.deadline(m_driveTrain.followPath(PathFactory.getPath(driveStartPoint, sourcePoint, m_isProcessorSide)),
+                        Commands.deadline(m_driveTrain.followPath(PathFactory.getPath(driveStartPoint, m_sourcePoint, m_isProcessorSide)),
                                           new WaitCommand(START_INTAKE_AFTER_PATH_START).andThen(
                                               new StartEndCommand(m_coralEffector::runIntake, m_coralEffector::stop, m_coralEffector)
                                                     .until(m_coralEffector::hasCoral).withTimeout(CORAL_PICKUP_WAIT_TIME))
